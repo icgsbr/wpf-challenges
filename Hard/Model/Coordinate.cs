2d3305c63eeda2baa50bc93adf5d041c318da375
icgsbr <igorcostagomessouza@gmail.com>
2022-04-18 11:38:17 -0300

    Hard test solution

---
+++ Hard/Model/Coordinate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Hard.Model
+{
+    public class Coordinate : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Point3D _position;
+
+        public Point3D Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                OnPropertyChanged("Coordinates");
+            }
+        }
+    }
+}
---
+++ Hard/ViewModel/CoordinateViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Hard.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Media.Media3D;
+
+namespace Hard.ViewModel
+{
+    public class CoordinateViewModel : ObservableCollection<Coordinate>
+    {
+        public string filePath = @"C:\Users\Usuario\Downloads\TestPlus.txt";
+
+        public List<string> lines = new List<string>();
+
+        public List<Point3D> listCoordinate = new List<Point3D>();
+
+        public CoordinateViewModel()
+        {
+            PrepareCoordinateCollection();
+        }
+
+        public void PrepareCoordinateCollection()
+        {
+            lines = File.ReadAllLines(filePath).ToList();
+
+            foreach (string line in lines)
+            {
+                listCoordinate.Add(Point3D.Parse(line));
+            }
+
+            foreach (Point3D point in listCoordinate)
+            {
+                var coordinate = new Coordinate
+                {
+                    Position = point
+                };
+                Add(coordinate);
+            }
+        }
+    }
+}
